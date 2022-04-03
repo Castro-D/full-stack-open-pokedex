8583fcef5b4f17f86d832931097195f12c255b0a
Castro-D <castro.dh91@gmail.com>
2022-04-03 19:13:16 -0300

    add navigation test

--- cypress/integration/e2etest.js
+++ cypress/integration/e2etest.js
@@ -6,4 +6,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate to a pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-container>a').eq(3).click()
+    cy.contains('charmander')
+  })
 })
